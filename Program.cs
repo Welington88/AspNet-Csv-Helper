3d40fa65ba9d8b7a20d35d0d1cca0dc3d7ac89b8
Welington Marquezini Valverde da Silva <welington_marquezini88@live.com>
2022-04-10 01:12:27 -0300

    ler o arquivo

--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Globalization;
+using System.IO;
+using CsvHelper;
+using CsvHelper.Configuration;
 
 namespace projetoCsvHelper
 {
@@ -6,7 +10,27 @@ namespace projetoCsvHelper
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var path = Path.Combine(Environment.CurrentDirectory, "input", "cid.csv");
+
+            var fi = new FileInfo(path);
+
+            if (!fi.Exists)
+            {
+                throw new FileNotFoundException($"Arquivo {path} Não Existe...");
+            }
+
+            using(var sr = new StreamReader(fi.FullName)) {
+                var csvConfig = new CsvConfiguration(CultureInfo.InstalledUICulture);
+                var csvReader = new CsvReader(sr,csvConfig);
+
+                var registros = csvReader.GetRecords<dynamic>();
+                foreach (var registro in registros)
+                {
+                    Console.WriteLine($"CID: {registro.CID}");
+                    Console.WriteLine($"Descrição: {registro.DESCR}");
+                    Console.WriteLine("---------------------------------------------");
+                }
+            } 
         }
     }
 }
