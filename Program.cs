55fa527e5846a0a88f46072a14511f323cc3dbbb
Welington Marquezini Valverde da Silva <welington_marquezini88@live.com>
2022-04-10 00:30:25 -0300

    inicial

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace projetoCsvHelper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
